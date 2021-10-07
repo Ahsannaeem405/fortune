7178d6d0fe055181b95fef1f8d546ffe3229d3a2
khuramnaqvi <89009279+khuramnaqvi@users.noreply.github.com>
2021-10-07 12:21:03 +0500

    done

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,8 +7,10 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-      {{-- fontawesome --}}
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+
+
+     
     <title>Fortune</title>
   </head>
   <style>
