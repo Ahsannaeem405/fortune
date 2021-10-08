6c8aa58cfe7b8265b4cf1b1cecc1ec9b6be088bd
khuramnaqvi <89009279+khuramnaqvi@users.noreply.github.com>
2021-10-08 17:20:51 +0500

    Update 2021_10_08_104157_add_paid_to_users_table.php

--- database/migrations/2021_10_08_104157_add_paid_to_users_table.php
+++ database/migrations/2021_10_08_104157_add_paid_to_users_table.php
@@ -14,7 +14,7 @@ class AddPaidToUsersTable extends Migration
     public function up()
     {
        Schema::table("users", function (Blueprint $table) {
-            $table->string('facebook_id');
+            $table->string('facebook_id')->nullable();
         });
     }
 
